2f2a9e725e00edc78a51fb6e35f31272f31ddedf
Rubens <105253719+JonataRubens@users.noreply.github.com>
2024-09-06 23:46:22 -0300

    Delete Interpolacao/interVizinho.py

--- Interpolacao/interVizinho.py
+++
@@ -1,8 +0,0 @@
-import cv2
-imagem = cv2.imread(r"C:\Users\WEB\Pictures\wallpaperflare.com_wallpaper (1).jpg")
-
-imagem_ampliada = cv2.resize(imagem, None, fx=2, fy=2, interpolation=cv2.INTER_NEAREST)
-imagem_reduzida = cv2.resize(imagem, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_NEAREST)
-
-cv2.imwrite('ampliada_vizinho.jpg', imagem_ampliada)
-cv2.imwrite('reduzida_vizinho.jpg', imagem_reduzida)
