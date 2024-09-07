a557dc6861dcfd3a9286f4eec86361d974031a9c
Rubens <105253719+JonataRubens@users.noreply.github.com>
2024-09-06 23:46:14 -0300

    Delete Interpolacao/interBilinear.py

--- Interpolacao/interBilinear.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-imagem = cv2.imread(r"C:\Users\WEB\Pictures\wallpaperflare.com_wallpaper (1).jpg")
-imagem_ampliada = cv2.resize(imagem, None, fx=2, fy=2, interpolation=cv2.INTER_LINEAR)
-
-cv2.imwrite(r"ampliada_bilinear.jpg", imagem_ampliada)  
-imagem_reduzida = cv2.resize(imagem, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_LINEAR)
-cv2.imwrite(r"reduzida_bilinear.jpg", imagem_reduzida)
-
